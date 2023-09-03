5505e7abfe637f00183a3ac91deaa505a34881d3
Diogo Melo <diogomelo500@gmail.com>
2023-09-02 21:32:30 -0300

    Alterações muito massa e adição de um database.json

---
+++ Projeto Final/classes.js
@@ -0,0 +1,83 @@
+class User {
+    constructor(name, balance = 0, canWork = Date.now(), isAdmin = false) {
+        this.name = name;
+        this.balance = balance;
+        this.canWork = canWork;
+        this.isAdmin = isAdmin;
+    }
+    work(amount) {
+        if (this.canWork + 5000 <= Date.now()) {
+            this.balance += amount;
+            this.canWork = Date.now();
+            return true;
+        }
+        return false;
+    }
+    transfer(amount, targetUser) {
+        this.balance -= amount;
+        targetUser.balance += amount;
+    }
+    bet(amount, number) {
+        const randomNumber = Math.floor(Math.random() * 10 + 1);
+        if (randomNumber === number) {
+            this.balance += amount * number;
+            return {
+                result: true,
+                amount,
+                balance: this.balance,
+            };
+        } else {
+            this.balance -= amount;
+            return {
+                result: false,
+                amount,
+                balance: this.balance,
+            };
+        }
+    }
+    get userInfos() {
+        return {
+            name: this.name,
+            balance: this.balance,
+        };
+    }
+}
+class Users extends Map {
+    addUser(key, user) {
+        const [obj] = user;
+        const newUser = new User(
+            obj.name,
+            obj.balance,
+            obj.canWork,
+            obj.isAdmin
+        );
+
+        this.set(key, newUser);
+    }
+    find(callback) {
+        let foundUser;
+        this.forEach((user, key) => {
+            const result = callback(user);
+            if (result) {
+                return (foundUser = key);
+            }
+        });
+        return this.get(foundUser);
+    }
+    saveUsers() {
+        const arrayUsers = [];
+
+        for (const [key, value] of this) {
+            const obj = {
+                [key]: value,
+            };
+            arrayUsers.push(obj);
+        }
+        return arrayUsers;
+    }
+}
+
+module.exports = {
+    User,
+    Users,
+};
--- Projeto Final/index.js
+++ Projeto Final/index.js
@@ -1,6 +1,16 @@
 // importamos a biblioteca discord.js
 const { Client, GatewayIntentBits } = require("discord.js");
-const { useInflection } = require("sequelize");
+const { User, Users } = require("./classes");
+const fs = require("fs");
+
+const registeredUsers = new Users();
+
+fs.readFile(`${__dirname}/database.json`, "utf8", (err, data) => {
+    if (err) throw err;
+    Array.prototype.forEach.call(JSON.parse(data), (user) => {
+        registeredUsers.addUser(Object.keys(user)[0], Object.values(user));
+    });
+});
 
 // criamos uma instancia do cliente discord.js
 // o construtor da classe Client recebe um objeto como parametro
@@ -30,114 +40,71 @@ client.on("messageCreate", (message) => {
     const conteudo = message.content; // exemplo de acesso ao conteudo da mensagem
     const autor = message.author; // exemplo de acesso ao autor da mensagem
     const user = registeredUsers.get(autor.id);
-    if (conteudo === "!Assinar CLT") {
+
+    if (conteudo === "!enviar curriculo") {
         // message.channel é um atributo que contém uma instancia da classe TextChannel (veja na documentação do discord.js)
         // esta instancia representa o canal onde a mensagem foi postada
         // ela tambem possui o metodo "send()", que permite postar uma mensagem no canal em questão, e assim responder á mensagem postada
-        if (registeredUsers.has(autor.id)) {
-            message.channel.send(`Você já assinou a CLT!`);
+        if (!!user) {
+            message.channel.send(`Você já está empregado!`);
         } else {
             registeredUsers.set(autor.id, new User(autor.username));
-            message.channel.send(`Você assinou a CLT!`);
+            message.channel.send(`Você foi contratado!`);
         }
     }
-    if (conteudo === "!Trabalhar") {
-        if (registeredUsers.has(autor.id)) {
-            user.work(100);
-            message.channel.send(`Você trabalhou eu ganhou 100 dinheiros!`);
-        } else {
-            message.channel.send(`Você ainda não assinou a CLT!`);
-        }
-    }
-    if (conteudo.includes("!Transferir")) {
-        if (registeredUsers.has(autor.id)) {
-            const [command, nameTargetUser, getValue] = conteudo.split(" ");
-            const targetUser = registeredUsers.find(
-                (user) => user.name === nameTargetUser
+    if (conteudo === "!trabalhar" && !!user) {
+        if (!user.work(100)) {
+            message.channel.send(
+                `Você já trabalhou demais, descance um pouco!`
             );
-            const value = Number(getValue);
-            if (targetUser && user.balance >= value) {
-                user.transfer(value, targetUser);
-                message.channel.send(
-                    `Você transferiu ${value} dinheiros para ${targetUser.name}`
-                );
-            } else {
-                message.channel.send(
-                    "Você não tem dinheiro suficiente ou o destinatario não existe"
-                );
-            }
+        } else {
+            message.channel.send(`Você trabalhou e ganhou 100 kwanzas!`);
         }
     }
-    if (conteudo.includes("apostar"))
-        if (registeredUsers.has(autor.id)) {
-            const [command, getValue, getNumber] = conteudo.split(" ");
-            const value = Number(getValue);
-            const number = Number(getNumber);
-            const results = user.bet(value, number);
-            if (results.result) {
-                message.channel.send(
-                    `Você apostou ${value} e ganhou ${
-                        results.amount * number
-                    } dinheiros!`
-                );
-            } else {
-                message.channel.send(
-                    `Você apostou ${value} e perdeu ${results.amount} dinheiros!`
-                );
-            }
+    if (conteudo.includes("!transferir") && !!user) {
+        const [, nameTargetUser, getValue] = conteudo.split(" ");
+        const targetUser = registeredUsers.find(
+            (user) => user.name === nameTargetUser
+        );
+        const value = Number(getValue);
+        if (targetUser && user.balance >= value) {
+            user.transfer(value, targetUser);
+            message.channel.send(
+                `Você transferiu ${value} kwanzas para ${targetUser.name}`
+            );
+        } else {
+            message.channel.send(
+                "Você não tem dinheiro suficiente ou o destinatario não existe"
+            );
         }
+    }
+    if (conteudo.includes("!apostar") && !!user) {
+        const [, getValue, getNumber] = conteudo.split(" ");
+        const value = Number(getValue);
+        const number = Number(getNumber);
+        const results = user.bet(value, number);
+        if (results.result) {
+            message.channel.send(
+                `Você apostou ${value} e ganhou ${
+                    results.amount * number
+                } kwanzas!`
+            );
+        } else {
+            message.channel.send(
+                `Você apostou ${value} e perdeu ${results.amount} kwanzas!`
+            );
+        }
+    }
+    if (conteudo === "!save" && user.isAdmin) {
+        fs.writeFileSync(
+            `${__dirname}/database.json`,
+            JSON.stringify(registeredUsers.saveUsers())
+        );
+        message.channel.send("Salvo com sucesso!");
+    }
 });
 
 // aqui iniciamos o login do discord, fazendo com que ele conecte e ative o seu sistema de eventos que criamos acima.
 client.login(
     "MTE0NzU2OTkyMTAyNzQ3MzU1OQ.GVmadc.pbiEvA6iwhsPRINeQTh47y2Vq1l9uZ8oq1jLt0"
 );
-
-class User {
-    constructor(name) {
-        this.name = name;
-        this.balance = 0;
-    }
-    work(amount) {
-        this.balance += amount;
-    }
-    transfer(amount, targetUser) {
-        this.balance -= amount;
-        targetUser.balance += amount;
-    }
-    bet(amount, number) {
-        const randomNumebr = Math.floor(Math.random() * 10 + 1);
-        if (randomNumebr === number) {
-            this.balance += amount * number;
-            return {
-                result: true,
-                amount: amount,
-                balance: this.balance,
-            };
-        } else {
-            this.balance -= amount;
-            return {
-                result: false,
-                amount: amount,
-                balance: this.balance,
-            };
-        }
-    }
-}
-class Users extends Map {
-    static addUser(user) {
-        this.set(user.name, user);
-    }
-    find(callback) {
-        let foundUser;
-        this.forEach((user, key) => {
-            const result = callback(user);
-            if (result) {
-                return (foundUser = key);
-            }
-        });
-        return this.get(foundUser);
-    }
-}
-
-const registeredUsers = new Users();
